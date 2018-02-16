53553ead4d80195cfaae878aae65fad654398365
phamminhquang <phamminhquangnt07@gmail.com>
2018-02-16 17:36:27 +0100

    Update train_dual_Q_value_shared.sh

--- script/train_dual_Q_value_shared.sh
+++ script/train_dual_Q_value_shared.sh
@@ -1,18 +1,14 @@
 index=$1
 device=$2
-faiss=~/faiss
 nematus=~/nematus-master
 source ~/anaconda3/bin/activate nmt_env
 MYPYTHON=~/anaconda3/envs/nmt_env/bin/
 MYPYLIB=~/anaconda3/envs/nmt_env/lib/
 export PATH=$MYPYTHON:${PATH}
-export PYTHONPATH=$MYPYLIB:$faiss:$nematus:$MYPYLIB/python2.7:$MYPYLIB/python2.7/dists-packages
-#export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64/:/DEV/cuda/lib64/:/home/klein/lib/cuda/lib64/
-export CPATH=/home/shared/lib/cuDNNv5/include:$CPATH
-export LIBRARY_PATH=/home/shared/lib/cuDNNv5/lib64:$LD_LIBRARY_PATH
+export PYTHONPATH=$MYPYLIB:$nematus:$MYPYLIB/python2.7:$MYPYLIB/python2.7/dists-packages
+export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
 export CUDA_VISIBLE_DEVICES=$device
-python ~/faiss/swigfaiss.py
+
 DIR=~/Dual_NMT/models/dual$index
 if [ ! -d "$DIR" ]; then
 echo $DIR
@@ -20,8 +16,5 @@ mkdir $DIR
 fi
 
 which python
-python -c 'import faiss; print "faiss OK"; import numpy; print "numpy OK"; import theano; print "theano OK"; import nematus; print "nematus OK"'
-
-#export DEVICE=cuda$device
-
+python -c 'import numpy; print "numpy OK"; import theano; print "theano OK"; import nematus; print "nematus OK"'
 THEANO_FLAGS='mode=FAST_RUN,optimizer=fast_run,device=gpu,floatX=float32,on_unused_input=warn' python -u /home/minhquang.pham/Dual_NMT/config_Q_value_$index.py
