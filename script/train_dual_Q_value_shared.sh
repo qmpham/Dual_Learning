7c7931d6d7c8aa9d98a017a77c37f5604979d2bb
PHAM Minh Quang <minhquang.pham@ssaling12.systran.local>
2018-02-16 17:37:08 +0100

    fgf

--- script/train_dual_Q_value_shared.sh
+++ script/train_dual_Q_value_shared.sh
@@ -1,18 +1,18 @@
 index=$1
 device=$2
-faiss=~/faiss
+#faiss=~/faiss
 nematus=~/nematus-master
 source ~/anaconda3/bin/activate nmt_env
 MYPYTHON=~/anaconda3/envs/nmt_env/bin/
 MYPYLIB=~/anaconda3/envs/nmt_env/lib/
 export PATH=$MYPYTHON:${PATH}
-export PYTHONPATH=$MYPYLIB:$faiss:$nematus:$MYPYLIB/python2.7:$MYPYLIB/python2.7/dists-packages
+export PYTHONPATH=$MYPYLIB:$nematus:$MYPYLIB/python2.7:$MYPYLIB/python2.7/dists-packages
 #export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-8.0/lib64/:/DEV/cuda/lib64/:/home/klein/lib/cuda/lib64/
 export CPATH=/home/shared/lib/cuDNNv5/include:$CPATH
 export LIBRARY_PATH=/home/shared/lib/cuDNNv5/lib64:$LD_LIBRARY_PATH
 export CUDA_VISIBLE_DEVICES=$device
-python ~/faiss/swigfaiss.py
+#python ~/faiss/swigfaiss.py
 DIR=~/Dual_NMT/models/dual$index
 if [ ! -d "$DIR" ]; then
 echo $DIR
@@ -20,7 +20,7 @@ mkdir $DIR
 fi
 
 which python
-python -c 'import faiss; print "faiss OK"; import numpy; print "numpy OK"; import theano; print "theano OK"; import nematus; print "nematus OK"'
+python -c 'import numpy; print "numpy OK"; import theano; print "theano OK"; import nematus; print "nematus OK"'
 
 #export DEVICE=cuda$device
 
